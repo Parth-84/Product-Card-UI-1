c7ced6b0df740ae31b363aebe78bb8f326bc9055
Parth <parthparmar25.pp@gmail.com>
2022-08-19 19:16:28 +0530

    Javascript makes life easier - removed redundancy and optimized code by removing same methods

--- card1.js
+++ card1.js
@@ -1,104 +1,58 @@
-let img = document.querySelector('#product-img');
-let img2 = document.querySelector('#product-img2');
-let img3 = document.querySelector('#product-img3');
 
-let color1 = document.getElementById('c1');
-let color2 = document.getElementById('c2');
-let color3 = document.getElementById('c3');
+let mainColorTarget,sibling1, sibling2;
+let imgbox;
 
-let size1 = document.getElementById('s1');
-let size2 = document.getElementById('s2');
-let size3 = document.getElementById('s3');
-let size4 = document.getElementById('s4');
 
-let allsizespan =  document.querySelectorAll('.size-class');
-let allsizespan2 =  document.querySelectorAll('.size-class2');
-let allcolorspan =  document.querySelectorAll('.color-class');
-let allcolorspan2 =  document.querySelectorAll('.color-class2');
-console.log(allsizespan);  
-console.log(allcolorspan);  
-function changeImage2(params){
-    img2.src = params;
-}
-function changeImage3(params){
-    img3.src = params;
+let mainSizeTarget, sizeParentNode;
+
+function changeImage(imgsrc,imgbox,e){
+mainColorTarget = e.target;
+
+getSpans(mainColorTarget);
+// console.log(mainTarget,sibling1,sibling2);
+mainColorTarget.classList.add('color-active');
+sibling1.classList.remove('color-active');
+sibling2.classList.remove('color-active');
+
+imgbox = document.getElementById(`${imgbox}`);
+imgbox.src = imgsrc;
+
 }
 
-function addClass(event){
-    allsizespan[0].classList.remove('size-active');
-    allsizespan[1].classList.remove('size-active');
-    allsizespan[2].classList.remove('size-active');
-    allsizespan[3].classList.remove('size-active');
-    let x = event.currentTarget;
-    x.className += " size-active";
-}
-function addClass2(event){
-    allsizespan2[0].classList.remove('size-active');
-    allsizespan2[1].classList.remove('size-active');
-    allsizespan2[2].classList.remove('size-active');
-    allsizespan2[3].classList.remove('size-active');
-    let x = event.currentTarget;
-    x.className += " size-active";
-}
-function addColorClass(event){
-    allcolorspan[0].classList.remove('color-active');
-    allcolorspan[1].classList.remove('color-active');
-    allcolorspan[2].classList.remove('color-active');
-    let x = event.currentTarget;
-    x.className += " color-active";
-}
-function addColorClass2(event){
-    allcolorspan2[0].classList.remove('color-active');
-    allcolorspan2[1].classList.remove('color-active');
-    allcolorspan2[2].classList.remove('color-active');
-    let x = event.currentTarget;
-    x.className += " color-active";
-}
 
-color1.addEventListener('click',()=>{
-    img.src = './Images/card1/card1_1.png';
-    color1.classList.add('color-active');
-    color2.classList.remove('color-active');
-    color3.classList.remove('color-active');
+
+//Getting target siblings
+function getSpans(mainColorTarget){
+    if(mainColorTarget.nextElementSibling){
+        sibling1 = mainColorTarget.nextElementSibling;
+        if(sibling1.nextElementSibling){
+            sibling2 = sibling1.nextElementSibling;
+        }
+        else{
+            sibling2 = mainColorTarget.previousElementSibling;
     
-});
-color2.addEventListener('click',()=>{
-    img.src = './Images/card1/card1_2.png';
-    color1.classList.remove('color-active');
-    color2.classList.add('color-active');
-    color3.classList.remove('color-active');
-});
-color3.addEventListener('click',()=>{
-    img.src = './Images/card1/card1_3.png';
-    color1.classList.remove('color-active');
-    color2.classList.remove('color-active');
-    color3.classList.add('color-active');
-});
+        }
+    }
+    else{
+        sibling1 = mainColorTarget.previousElementSibling;
+        sibling2 = sibling1.previousElementSibling;
+    }
+
+}
 
 
-size1.addEventListener('click',()=>{
-    size1.classList.add('size-active');
-    size2.classList.remove('size-active');
-    size3.classList.remove('size-active');
-    size4.classList.remove('size-active');
-    
-});
-size2.addEventListener('click',()=>{
-    size1.classList.remove('size-active');
-    size2.classList.add('size-active');
-    size3.classList.remove('size-active');
-    size4.classList.remove('size-active');
-});
-size3.addEventListener('click',()=>{
-    size1.classList.remove('size-active');
-    size2.classList.remove('size-active');
-    size3.classList.add('size-active');
-    size4.classList.remove('size-active');
-});
-size4.addEventListener('click',()=>{
-    size1.classList.remove('size-active');
-    size2.classList.remove('size-active');
-    size3.classList.remove('size-active');
-    size4.classList.add('size-active');
-});
 
+//Changing size function.
+function changeSize(e){
+    mainSizeTarget = e.target;
+    sizeParentNode = mainSizeTarget.parentNode;
+    let spans = Array.from(sizeParentNode.getElementsByTagName('span'));
+    // console.log(spans);
+
+    spans.forEach(element=>{
+        element.classList.remove('size-active');
+    });
+    mainSizeTarget.classList.add('size-active');
+}
+
+//Image changing function can also implemented like size changing function.
